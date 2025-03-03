5a95742c366acaee6bb492f764493a78c6b05a26
Gabriel Wilkins <wilkigab001@gmail.com>
2025-03-02 21:35:50 -0700

    JUst testing this out to see what it would like on the website

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const CurrentYear = new Date().getFullYear();
+document.querySelector('#currentyear').textContent = CurrentYear
+
+const LastModified = document.lastModified;
+document.querySelector('#lastmodified').textContent = LastModified
